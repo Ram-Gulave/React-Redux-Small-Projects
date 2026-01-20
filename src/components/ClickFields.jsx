891e841d0064db680c2827513d87c118c40d35c4
Ram Gulave <gulaveram2@gmail.com>
2026-01-20 20:28:38 +0530

    new

--- src/components/ClickFields.jsx
+++ src/components/ClickFields.jsx
@@ -1,20 +1,40 @@
-import { useEffect, useRef } from "react"
+import { useEffect, useRef,useState } from "react"
 
 const ClickFields = () => {
-        const inputref = useRef(null)
-        
-        useEffect(() => {
-          inputref.current = setInterval(() => {
-            console.log("Timer running");
-            
-          }, 1000)
-        
-          return () => {
-            clearInterval(inputref.current)
-          }
-        }, [])
-        
-    return <p>Click the console for timer output...</p>
+	const [count, setCount] = useState(0)
+	let val = useRef(0);
+	let btnRef = useRef();
+	// let val = 0   // value is not persists on a every re-render. When rendering happens whole code excute from start to end.
+
+	const handleClick = () => {
+		val.current = val.current + 1;
+		console.log("value of a current ",val.current);		
+		setCount(count + 1);
+	}
+
+	useEffect(() => {
+		console.log("Re-render on every trigger....");
+	})
+
+	const handleColor = () => {
+		btnRef.current.style.backgroundColor = "green";
+	}
+	// useEffect(() => {
+	//   inputref.current = setInterval(() => {
+	//     console.log("Timer running");
+	//   }, 1000)
+	//   return () => {
+	//     clearInterval(inputref.current)
+	//   }
+	// }, [])
+
+	return (
+		<div>
+			<button className="add-to-cart-btn" ref={btnRef} onClick={handleClick}>Increment</button>
+			<button className="btn" onClick={handleColor} >Chnage Color</button>
+			<p className="card-content">{count}</p>
+		</div>
+	)
 }
 
 export default ClickFields
